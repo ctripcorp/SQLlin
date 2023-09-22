f02256cea20f019e6fe88cf703e5c5542f5514ee
qiaoyuang <qiaoyuang2012@gmail.com>
2023-09-22 15:41:48 +0800

    Modify the android instrumented tests shell script

--- test_android.sh
+++ test_android.sh
@@ -1,5 +1,5 @@
 #Run Android instrumented tests
-adb shell pm clear com.ctrip.sqllin.driver.test
-adb shell pm clear com.ctrip.sqllin.dsl.test
 ./gradlew :sqllin-driver:connectedDebugAndroidTest --stacktrace
 ./gradlew :sqllin-dsl:connectedDebugAndroidTest --stacktrace
+adb uninstall com.ctrip.sqllin.driver.test
+adb uninstall com.ctrip.sqllin.dsl.test
