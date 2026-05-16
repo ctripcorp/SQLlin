5b7c232c4a7560a29277fa7d2af15fdcd72d57f1
qiaoyuang <qiaoyuang2012@gmail.com>
2026-05-16 19:44:49 +0100

    Update Android test commands to use `connectedAndroidDeviceTest`

--- test_android.sh
+++ test_android.sh
@@ -1,5 +1,5 @@
 #Run Android instrumented tests
-./gradlew :sqllin-driver:connectedDebugAndroidTest --stacktrace
-./gradlew :sqllin-dsl-test:connectedDebugAndroidTest --stacktrace
+./gradlew :sqllin-driver:connectedAndroidDeviceTest --stacktrace
+./gradlew :sqllin-dsl-test:connectedAndroidDeviceTest --stacktrace
 #adb uninstall com.ctrip.sqllin.driver.test
 #adb uninstall com.ctrip.sqllin.dsl.test
